16fb90e90231be47cb92e5fb7dfb34ecefcf2496
Nirob2107092 <perfeccnirob@gmail.com>
2026-03-19 13:51:45 +0600

    feat(dto): added cart and order flow DTOs

---
+++ src/main/java/com/pc/pc/dto/PlaceOrderRequestDto.java
@@ -0,0 +1,13 @@
+package com.pc.pc.dto;
+
+public class PlaceOrderRequestDto {
+    private Long buyerId;
+
+    public Long getBuyerId() {
+        return buyerId;
+    }
+
+    public void setBuyerId(Long buyerId) {
+        this.buyerId = buyerId;
+    }
+}
